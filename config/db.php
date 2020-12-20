b931f6ba2d165b69e3d095491285525b02e096b6
dvillegas1987 <dvillegas.soft@gmail.com>
2020-12-20 01:27:29 -0300

    primera subida

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:3306;dbname=way',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+
+
+/*return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=mcneuquen',
+    'username' => 'ciudadania',
+    'password' => 'ciudadania',
+    'charset' => 'utf8',
+];*/
